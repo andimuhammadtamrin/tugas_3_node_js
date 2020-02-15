0d2fda8e84c97f50bf77255eef482dd7b51c98a7
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2020-02-15 17:28:14 +0800

    Tugas 3  Node JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ resthub/api-routes.js
@@ -0,0 +1,27 @@
+//initialize express router
+let router = require('express').Router();
+
+//set default API response
+router.get('/',function(req,res){
+  res.json({
+    status: 'Api its working',
+    message: 'Welcome to Resthub crafted'
+  });
+});
+
+//import contactControler
+var siswaControler = require('./siswaControler');
+
+//contact apiRoutes
+router.route('/siswa')
+  .get(siswaControler.index)
+  .post(siswaControler.new);
+
+router.route('/siswa/:sis_id')
+  .get(siswaControler.view)
+  .patch(siswaControler.update)
+  .put(siswaControler.update)
+  .delete(siswaControler.delete);
+
+//express Api
+module.exports = router;
